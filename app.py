507646d4225bd74c24b58292663eb00a870e515f
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2015-07-20 18:12:22 -0400

    heroku ready

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import string
 import random
-from flask import Flask, request, redirect, url_for, render_template
-from models import *
+import sqlite3
+from flask import Flask, request, redirect, url_for, render_template, g
 
 app = Flask(__name__)
 
@@ -11,6 +11,43 @@ digits = string.digits
 chars = uppers + lowers + digits
 root_url = "localhost:5000/"
 url_dict = {"empty" : {"url" : "empty", "views" : 0}}
+
+DATABASE = 'database.db'
+
+def insert_url(key,url,views):
+    con = get_db()
+    cur = con.cursor()
+    cur.execute("INSERT INTO entries (key,url,views) VALUES (?,?,?)", (key,url,views))
+    con.commit()
+    con.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def connect_to_database():
+    return sqlite3.connect(DATABASE)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route('/')
 def index():
